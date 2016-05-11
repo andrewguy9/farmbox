9047f22676881c754bd26e015df43b6d7e6ccb6f
Andrew Thomson <athomsonguy@gmail.com>
2016-05-11 05:07:09 +0000

    Initial commit.

---
+++ farmfs/manifests/init.pp
@@ -0,0 +1,53 @@
+class farmfs {
+
+  package { "usbmount":
+    ensure => installed,
+  } ->
+  file { '/etc/usbmount/usbmount.conf':
+    ensure => file,
+    content => file('farmfs/usbmount.conf'),
+  }
+  #exec  { 'chown_usb_files':
+  #  # User IDs are different from host to host.
+  #  timeout => 0,
+  #  command => '/bin/chown -R pi /media/*',
+  #  unless => '/usr/bin/test `find /media/* ! -user pi | wc -l` == 0',
+  #} 
+  
+  package { "xfsprogs":
+    ensure => installed,
+  }
+  
+  file { '/usr/local/bin/now':
+    ensure => file,
+    content => "#!/bin/bash\n date \"+%Y-%m-%d-%H:%M:%S\"",
+    mode => '555',
+  }
+
+  package {'python-pip':
+    ensure => installed,
+  }
+  
+  package { 'farmfs': 
+    require => Package['python-pip'],
+    ensure  => latest,
+    provider => pip,
+  }
+
+  package { 'samba':
+    ensure => installed,
+  } ->
+  file { '/etc/samba/smb.conf':
+    ensure => file,
+    content => file('farmfs/smb.conf'),
+  } ->
+  service { 'samba':
+    hasrestart => true,
+    ensure => running,
+  }
+
+  package { 'screen':
+    ensure => installed,
+  }
+}
+
