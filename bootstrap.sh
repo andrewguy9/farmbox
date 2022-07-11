df29f701a055679be62b67d207fc0954bd325a73
Andrew Thomson <athomsonguy@gmail.com>
2022-07-11 11:34:47 -0700

    switch to puppet root package for bootstrapping.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get -y install git
-apt-get -y install puppet-common
+apt-get -y install puppet
 git clone https://github.com/andrewguy9/farmbox.git
 cd farmbox
 puppet apply  --modulepath=. ./farmbox.pp
